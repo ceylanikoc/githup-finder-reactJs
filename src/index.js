47e87e4d025bb263fa372d578b20fea9fae7d8e7
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-10 19:09:23 +0300

    RouteLink, Link NavLink Used

--- src/index.js
+++ src/index.js
@@ -2,26 +2,56 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/main.scss";
 import App from "./components/App";
-import { BrowserRouter,Route,Switch } from "react-router-dom";
+import { BrowserRouter,Route,Switch,Link,NavLink } from "react-router-dom";
+
+const Nav = () => (
+        // <nav>
+        //     <Link to="/">Home</Link> |
+        //     <Link to="/contact">Contact</Link> |
+        //     <Link to="/products">Products</Link> 
+        // </nav>
+        
+        <nav>
+            <NavLink exact to="/" activeClassName="active">Home</NavLink> |
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink> |
+            <NavLink to="/products" activeClassName="active">Products</NavLink> 
+        </nav>
+)
+
+const Header = () =>  (
+    <header>
+        <h1>Githup Finder</h1>
+    </header>
+);
 
 const HomePage = () => (
-    <div>Home Page</div>
+    <>
+        <div>Home Page</div>
+    </>
 );
 
 const ContactPage = () => (
-    <div>Contact Page</div>
+    <>
+        <div>Contact Page</div>
+    </>
 );
 
 const ProductPage = () => (
-    <div>Product Page</div>
+    <>
+        <div>Producs Page</div>
+    </>
 );
 
 const NotFound = () => (
-    <div>404 Page</div>
+    <>
+        <div>404 Page</div>
+    </>
 );
 
 const routes = (
     <BrowserRouter>
+        <Header />
+        <Nav/>
         <Switch>
             <Route exact path="/" component = { HomePage} />
             <Route path="/contact" component = { ContactPage} />
