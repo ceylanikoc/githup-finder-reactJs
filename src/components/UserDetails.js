dbb32a71587791ed573da804d472b63f68af38e3
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-13 15:37:45 +0300

    userdetails class component to stateless function component Hook useEffect used

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,69 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component,useEffect } from 'react'
 import Loading from './Loading'
 import Repos from './Repos'
 
-class UserDetails extends Component {
-  componentDidMount() {
-      this.props.getUser(this.props.match.params.login);
-      this.props.getUserRepos(this.props.match.params.login);
-  }
-  render() {
-    const {loading,repos} = this.props;
-    const {name,avatar_url,location,html_url,bio,blog,followers,following,public_repos} =this.props.user;
+const UserDetails = ({getUser,getUserRepos,match,loading,repos,user}) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+    },[])
+    const {name,avatar_url,location,html_url,bio,blog,followers,following,public_repos} =user;
+
     if(loading) {
         return <Loading />
     }else {
-        return (
-            <div className="container my-3">
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={avatar_url} alt="" className="card-img-top" />
-                            <div className="card-body">
-                                <p className="card-text">{name}</p>
-                                { location && 
-                                <p>
-                                    <i className="fas fa-map-marker-alt"></i> {location}
-                                </p>
-                                }
-                                <p>
-                                    <a href={html_url} className="btn btn-block btn-primary btn-sm" target="_blank">Githup Profile</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-9">
-                        <div className="card">
-                            <div className="card-body">
-                                {
-                                    bio && 
-                                    <>
-                                        <h3>About</h3>
-                                        <p>{bio}</p>
-                                    </>
-                                }
-                                {
-                                    blog &&
-                                        <>
-                                            <h3>Blog</h3>
-                                            <p>{blog}</p>
-                                        </>
-                                }
-                                <div>
-                                    <span className="badge badge-primary m-1">Followers : {followers} </span>
-                                    <span className="badge badge-danger m-1">Following : {following} </span>
-                                    <span className="badge badge-success m-1">Public Repos : {public_repos} </span>
-                                </div>
-                            </div>
-                            <ul className="list-group list-group-flush">
-                                <Repos repos={repos} />
-                            </ul>
+    return (
+        <div className="container my-3">
+            <div className="row">
+                <div className="col-md-3">
+                    <div className="card">
+                        <img src={avatar_url} alt="" className="card-img-top" />
+                        <div className="card-body">
+                            <p className="card-text">{name}</p>
+                            { location && 
+                            <p>
+                                <i className="fas fa-map-marker-alt"></i> {location}
+                            </p>
+                            }
+                            <p>
+                                <a href={html_url} className="btn btn-block btn-primary btn-sm" target="_blank">Githup Profile</a>
+                            </p>
                         </div>
                     </div>
                 </div>
+                <div className="col-md-9">
+                    <div className="card">
+                        <div className="card-body">
+                            {
+                                bio && 
+                                <>
+                                    <h3>About</h3>
+                                    <p>{bio}</p>
+                                </>
+                            }
+                            {
+                                blog &&
+                                    <>
+                                        <h3>Blog</h3>
+                                        <p>{blog}</p>
+                                    </>
+                            }
+                            <div>
+                                <span className="badge badge-primary m-1">Followers : {followers} </span>
+                                <span className="badge badge-danger m-1">Following : {following} </span>
+                                <span className="badge badge-success m-1">Public Repos : {public_repos} </span>
+                            </div>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <Repos repos={repos} />
+                        </ul>
+                    </div>
+                </div>
             </div>
-          )
-    }
+        </div>
+    )
   }
 }
 
